4ac1cab8def12184b467099045594ce760cf96e1
Irshad Ajmal Ahmed <iahmed@enova.com>
2017-08-01 12:02:05 -0500

    iteration 2

--- exercise-001-corpus/word_count.go
+++ exercise-001-corpus/word_count.go
@@ -3,57 +3,58 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strings"
 )
 
+type count struct {
+	word  string
+	count int
+}
+
+type byCount []count
+
+func (a byCount) Len() int           { return len(a) }
+func (a byCount) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byCount) Less(i, j int) bool { return a[i].count > a[j].count }
+
 func main() {
-	str, ok := readFromFile("7oldsamr.txt")
+	var str string
+	var ok bool
+	file, err := ioutil.ReadFile("7oldsamr.txt")
+	if err != nil {
+		str, ok = "", false
+	}
+	str, ok = string(file), true
 	if !ok {
 		fmt.Println("File error!")
 		return
 	}
-	words := make(map[string]int)
-	words = buildMap(str)
-	printInOrder(words)
-}
-
-func readFromFile(filename string) (string, bool) {
-	file, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return "", false
+	var words = buildCountArray(str)
+	sort.Sort(byCount(words))
+	//fmt.Printf("%+v\n", words)
+	for _, elem := range words {
+		fmt.Printf("%s\t%d\n", elem.word, elem.count)
 	}
-	return string(file), true
 }
 
 func buildMap(str string) map[string]int {
 	words := make(map[string]int)
-	wordsArray := strings.Fields(purify(str))
-	for i := 0; i < len(wordsArray); i++ {
-		if frequency, ok := words[wordsArray[i]]; ok { //key exists, increment frequency
-			words[wordsArray[i]] = frequency + 1
-		} else { //key does not exist, add to map with 1 frequency
-			words[wordsArray[i]] = 1
-		}
+	wordsList := strings.Fields(purify(str))
+	for _, word := range wordsList {
+		words[word]++
 	}
 	return words
 }
 
-func printInOrder(words map[string]int) {
-	initialLength := len(words)
-	for i := 0; i < initialLength; i++ {
-		max := 0
-		maxKey := ""
-		for key, val := range words {
-			if val > max {
-				maxKey = key
-				max = val
-			}
-			delete(words, maxKey)
-		}
-		if max > 0 {
-			fmt.Printf("\n%d : %s", max, maxKey)
-		}
+func buildCountArray(str string) []count {
+	var words = buildMap(str)
+	countsList := make([]count, 0)
+	for w, c := range words {
+		elem := count{word: w, count: c}
+		countsList = append(countsList, elem)
 	}
+	return countsList
 }
 
 func purify(str string) string {
