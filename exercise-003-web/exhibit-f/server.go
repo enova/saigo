14832e4584c2594b2b5ab7836be9fc121c0cf7aa
Deliana Escobari <descobari@enova.com>
2017-10-26 14:23:58 -0500

    Implement solution using a struct

--- exercise-003-web/exhibit-f/server.go
+++ exercise-003-web/exhibit-f/server.go
@@ -13,7 +13,7 @@ type User struct {
 }
 
 var homeT = template.Must(template.ParseFiles("exhibit-f/home.html"))
-var users map[string]int
+var users map[string]*User
 
 func home(w http.ResponseWriter, r *http.Request) {
 	homeT.Execute(w, nil)
@@ -24,21 +24,21 @@ func signup(w http.ResponseWriter, r *http.Request) {
 	username := r.Form.Get("username")
 
 	if _, ok := users[username]; ok {
-		users[username] = users[username] + 1
+		users[username].Submissions = users[username].Submissions + 1
 	} else {
-		users[username] = 1
+		users[username] = &User{username, 1}
 	}
 	// users = append(users, username)
 	fmt.Fprint(w, "People using this wonderful form: \n")
 	fmt.Fprint(w, "Name\tSubmissions\n")
-	for user, submissions := range users {
-		fmt.Fprintf(w, "%s\t%d\n", user, submissions)
+	for _, user := range users {
+		fmt.Fprintf(w, "%s\t%d\n", user.Name, user.Submissions)
 	}
 
 }
 
 func main() {
-	users = make(map[string]int)
+	users = make(map[string]*User)
 	http.HandleFunc("/home", home)
 	http.HandleFunc("/signup", signup)
 	http.ListenAndServe(":8080", nil)
