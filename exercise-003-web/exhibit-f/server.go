898e1afac566669bdebcb22e851234230a157507
Deliana Escobari <descobari@enova.com>
2017-10-30 14:20:02 -0500

    Create gob file to persist users and frequencies across shutdowns.

--- exercise-003-web/exhibit-f/server.go
+++ exercise-003-web/exhibit-f/server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/gob"
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+	"runtime"
 )
 
 //User that signs up for this great app
@@ -15,11 +19,17 @@ var homeT = template.Must(template.ParseFiles("exhibit-f/home.html"))
 var usersT = template.Must(template.ParseFiles("exhibit-f/users.html"))
 var users map[string]*User
 
+const filename = "exhibit-f/users.gob"
+
 func home(w http.ResponseWriter, r *http.Request) {
 	homeT.Execute(w, nil)
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
+
+	//load any prior data. For simplicity, don't check for errors becuse if the file is empty it will throw and EOF error.
+	load(filename, &users)
+
 	r.ParseForm()
 	username := r.Form.Get("username")
 
@@ -30,12 +40,58 @@ func signup(w http.ResponseWriter, r *http.Request) {
 	}
 
 	usersT.Execute(w, &users)
+
+	//save the updated variable in the same file.
+	err := save(filename, &users)
+	check(err)
+}
+
+// save Encode via Gob to file
+func save(path string, object interface{}) error {
+	file, err := os.Create(path)
+	if err == nil {
+		encoder := gob.NewEncoder(file)
+		encoder.Encode(object)
+	}
+	file.Close()
+	return err
+}
+
+// load Decode Gob file
+func load(path string, object interface{}) error {
+	file, err := os.Open(path)
+	if err == nil {
+		decoder := gob.NewDecoder(file)
+		err = decoder.Decode(object)
+	}
+	file.Close()
+	return err
+}
+
+// clears the data file
+func clear(w http.ResponseWriter, r *http.Request) {
+	users = make(map[string]*User)
+	homeT.Execute(w, nil)
+
+	//save the updated variable in the same file.
+	err := save(filename, &users)
+	check(err)
+}
+
+// check for errors
+func check(e error) {
+	if e != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Println(line, "\t", file, "\n", e)
+		os.Exit(1)
+	}
 }
 
 func main() {
 	users = make(map[string]*User)
 	http.HandleFunc("/", home)
 	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/clear", clear)
 	http.ListenAndServe(":8080", nil)
 
 }
