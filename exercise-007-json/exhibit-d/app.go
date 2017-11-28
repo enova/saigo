f7c1c070e97d3e8b46a5416c0fafb41c99a853fe
mrhectorcrackle <nick_turner9@hotmail.com>
2017-11-28 16:26:38 -0600

    exercise 7

--- exercise-007-json/exhibit-d/app.go
+++ exercise-007-json/exhibit-d/app.go
@@ -6,16 +6,24 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"database/sql"
+
+	_ "github.com/lib/pq"
 )
 
 // Phone ...
 type Phone struct {
-	Name string `json:"name"`
+	Name     string `json:"name"`
+	Age      int    `json:"age"`
+	Carrier  string `json:"carrier"`
+	Id 			 string `json:"id"`
+	ImageUrl string `json:"imageUrl"`
+	Snippet  string `json:"snippet"`
 }
 
 var allPhones []Phone
 
-func setup() {
+func addFromFile(db *sql.DB) {
 	data, err := ioutil.ReadFile("phones.json")
 	if err != nil {
 		fmt.Println("Error reading phones.json")
@@ -25,6 +33,45 @@ func setup() {
 	err = json.Unmarshal(data, &allPhones)
 	if err != nil {
 		fmt.Println("Error in unmarshalling phones")
+		os.Exit(1)
+	}
+	for _, phone := range allPhones {
+		_, err = db.Exec("INSERT INTO phones(id, name, age, carrier, imageUrl, snippet) VALUES ($1, $2, $3, $4, $5, $6)", phone.Id, phone.Name, phone.Age, phone.Carrier, phone.ImageUrl, phone.Snippet)
+		if err != nil {
+			fmt.Println("Error inserting lines to db")
+			os.Exit(1)
+		}
+	}
+}
+
+func setup() {
+	db, err := sql.Open("postgres", "dbname=saigo user=postgres password=postgres sslmode=disable")
+	if err != nil {
+		fmt.Println("Error connecting to db")
+		os.Exit(1)
+	}
+
+	rows, err := db.Query("SELECT * FROM phones")
+	if err != nil {
+		fmt.Println("Error running query")
+		os.Exit(1)
+	}
+	count := 0
+
+	for rows.Next() {
+		phone := Phone{}
+
+		err := rows.Scan(&phone.Id, &phone.Name, &phone.Age, &phone.Carrier, &phone.ImageUrl, &phone.Snippet)
+		if err != nil {
+			fmt.Println("Error scanning db row")
+			os.Exit(1)
+		}
+		allPhones = append(allPhones, phone)
+		count ++
+	}
+
+	if count == 0 {
+		addFromFile(db)
 	}
 }
 
