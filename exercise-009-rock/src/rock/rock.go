d15ed9eb65e475c3af666e219175fd9de02decd8
mrhectorcrackle <nick_turner9@hotmail.com>
2017-11-30 14:04:10 -0600

    bonus

--- exercise-009-rock/src/rock/rock.go
+++ exercise-009-rock/src/rock/rock.go
@@ -1,17 +1,11 @@
 package main
 
 func main() {
-
 	// Create Game
 	game := &Game{}
 
 	// Add Players
-	game.Add(&RandoRex{})
-	game.Add(&RandoRex{})
-	game.Add(&Flipper{Rock,Paper})
-	game.Add(&Obsessed{Paper})
-	game.Add(&Obsessed{Scissors})
-	game.Add(&Cyclone{})
+	game.AddPlayers()
 
 	// A Thousand Round-Robins!
 	for i := 0; i < 1000; i++ {
