7a64a118990a765f703480ccae046a0a7e157f90
mrhectorcrackle <nick_turner9@hotmail.com>
2017-12-01 15:12:04 -0600

    take 2

--- exercise-003-web/server.go
+++ exercise-003-web/server.go
@@ -7,16 +7,7 @@ import (
 	"net/http"
 )
 
-type User struct {
-	Name  string
-	Count int
-}
-
-type Users struct {
-	List []*User
-}
-
-var users Users
+var users map[string]int
 
 var homeT = template.Must(template.ParseFiles("home.html"))
 
@@ -31,17 +22,7 @@ func addName(w http.ResponseWriter, r *http.Request) {
 	name := r.Form.Get("name")
 	data, _ := ioutil.ReadFile("users.json")
 	_ = json.Unmarshal(data, &users)
-	match := false
-	for _, user := range users.List {
-		if name == user.Name {
-			user.Count++
-			match = true
-		}
-	}
-	if match != true {
-		userList := append(users.List, &User{name, 1})
-		users = Users{userList}
-	}
+ 	users[name] = users[name] + 1
 	b, _ := json.Marshal(users)
 	_ = ioutil.WriteFile("users.json", b, 0644)
 	homeT.Execute(w, &users)
