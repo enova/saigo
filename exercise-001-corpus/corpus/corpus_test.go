32655912841db34036f0a2f65ba8e5dd0032b08c
Mike Jahn (mjahn) <mjahn@enova.com>
2017-12-18 11:37:05 -0600

    Better naming for tests

--- exercise-001-corpus/corpus/corpus_test.go
+++ exercise-001-corpus/corpus/corpus_test.go
@@ -5,14 +5,14 @@ import (
 	"testing"
 )
 
-func TestCorpus(t *testing.T) {
+func TestCorpusShortString(t *testing.T) {
 	result := Analyze("Are you serious? I knew you were.")
 	assert.Equal(t, 6, len(result))
 	assert.Equal(t, "you", result[0].Word)
 	assert.Equal(t, 2, result[0].Count)
 }
 
-func TestCorpusAgain(t *testing.T) {
+func TestCorpusLongerString(t *testing.T) {
 	result := Analyze("Are you serious? I knew you were. What is going on? I don't know. Who knows? I know. I do know.")
 	assert.Equal(t, "I", result[0].Word)
 	assert.Equal(t, 4, result[0].Count)
