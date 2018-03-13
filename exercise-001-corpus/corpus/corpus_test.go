7a9648b39e3e92a6095085fa96f673244507fcaf
Pranav Moktali <pranav.moktali@gmail.com>
2018-03-13 16:48:18 -0500

    add corpus.Analyze

---
+++ exercise-001-corpus/corpus/corpus_test.go
@@ -0,0 +1,14 @@
+package corpus
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCorpus(t *testing.T) {
+	result := Analyze("Are you serious? I knew you were.")
+	assert.Equal(t, len(result), 6)
+	assert.Equal(t, result[0].Word, "you")
+	assert.Equal(t, result[0].Count, 2)
+}
