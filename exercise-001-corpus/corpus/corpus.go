71715a4052e11535f56ffd3715dee2dd5f9d07c3
Shiqi Wang <swang@enova.com>
2018-03-30 13:54:57 -0500

    first round of corpus

---
+++ exercise-001-corpus/corpus/corpus.go
@@ -0,0 +1,50 @@
+package corpus
+
+import (
+	"sort"
+	"strings"
+)
+
+type Pair struct {
+	Word string
+	Freq int
+}
+
+type PairList []Pair
+
+// implement sort interface
+func (this PairList) Len() int {
+	return len(this)
+}
+
+// deterministic, both Freq and Word should be taken into consideration
+func (this PairList) Less(i, j int) bool {
+	if this[i].Freq == this[j].Freq {
+		return this[i].Word < this[j].Word
+	}
+	return this[i].Freq < this[j].Freq
+}
+
+func (this PairList) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+
+func sortByFreq(dict map[string]int) PairList {
+	sorted := make(PairList, len(dict))
+	i := 0
+	for word, freq := range dict {
+		sorted[i] = Pair{word, freq}
+		i++
+	}
+	sort.Sort(sort.Reverse(sorted))
+	return sorted
+}
+
+func WordCount(s string) PairList {
+	words := strings.Fields(s)
+	dict := make(map[string]int)
+	for _, word := range words {
+		dict[strings.ToLower(word)] += 1
+	}
+	return sortByFreq(dict)
+}
