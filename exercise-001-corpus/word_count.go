58ae2923e538cd62ef21ea2226c720257d9a74df
Shiqi Wang <swang@enova.com>
2018-04-02 11:58:19 -0500

    replace os with ioutil

--- exercise-001-corpus/word_count.go
+++ exercise-001-corpus/word_count.go
@@ -3,27 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/CoderAlison/saigo/exercise-001-corpus/corpus"
-	"os"
+	"io/ioutil"
 )
 
 func main() {
-	file, err := os.Open("7oldsamr.txt")
-	if err != nil {
-		return
-	}
-
-	defer file.Close()
-
-	stat, err := file.Stat()
-	if err != nil {
-		return
-	}
-
-	bs := make([]byte, stat.Size())
-	_, err = file.Read(bs)
-	if err != nil {
-		return
-	}
+	bs, err := ioutil.ReadFile("7oldsamr.txt")
+  if err != nil {
+    return
+  }
 
 	dict := corpus.WordCount(string(bs))
 	for _, pair := range dict {
