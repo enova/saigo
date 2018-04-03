67d6267aec45fb60aca83b635985093177467a52
Shiqi Wang <swang@enova.com>
2018-04-03 16:27:30 -0500

    namebook server

---
+++ exercise-003-web/exercise/server.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+)
+
+type View struct {
+	NameBook map[string]int
+}
+
+var (
+	lock         sync.Mutex
+	namebook     map[string]int
+	homeT        = template.Must(template.ParseFiles("exercise/home.html"))
+	namebookPath = "exercise/namebook.json"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	v := View{NameBook: namebook}
+	homeT.Execute(w, &v)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := strings.TrimSpace(r.Form.Get("username"))
+	if username != "" {
+		namebook[username]++
+		js, _ := json.Marshal(namebook)
+		lock.Lock()
+		err := writeNamebook(js)
+		lock.Unlock()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	http.Redirect(w, r, "/home", 301)
+}
+
+func writeNamebook(j []byte) error {
+	err := ioutil.WriteFile(namebookPath, j, 0644)
+	return err
+}
+
+func readNamebook() error {
+	namebook = make(map[string]int)
+	_, err := os.Stat(namebookPath)
+	if os.IsNotExist(err) {
+		var file, errCreate = os.Create(namebookPath)
+		file.Close()
+		err = errCreate
+	} else if err == nil {
+		namebookFile, errOpen := os.Open(namebookPath)
+		err = errOpen
+		defer namebookFile.Close()
+		s, _ := ioutil.ReadFile(namebookPath)
+		err = json.Unmarshal(s, &namebook)
+	}
+	return err
+}
+
+func main() {
+	lock.Lock()
+	err := readNamebook()
+	lock.Unlock()
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/home", home)
+	http.HandleFunc("/signup", signup)
+	http.ListenAndServe(":8080", nil)
+}
