18456571c66100d8a41080780bd2aff66227afec
Bo Guthrie <bguthrie@enova.com>
2018-04-06 16:08:00 -0500

    Update word count to reflect mike's comments

--- exercise-001-corpus/word_count.go
+++ exercise-001-corpus/word_count.go
@@ -1,45 +1,35 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 
 	"github.com/bguthrie/saigo/exercise-001-corpus/corpus"
 )
 
-func checkEquality(ar1, ar2 []string) bool {
-	if ar1 == nil || ar2 == nil {
-		return false
+func main() {
+	args := os.Args
+	necessaryInputs := []string{"./word_count", "file.txt"}
+	errorMessage := "Please input: ./word_count file.txt"
+
+	if args == nil || necessaryInputs == nil {
+		panic(errorMessage)
 	}
 
-	if len(ar1) != len(ar2) {
-		return false
+	if len(args) != len(necessaryInputs) {
+		panic(errorMessage)
 	}
 
-	for i := range ar1 {
-		if ar1[i] != ar2[i] {
-			return false
+	for i := range args {
+		if args[i] != necessaryInputs[i] {
+			panic(errorMessage)
 		}
 	}
 
-	return true
-}
-
-func checkInput(args ...string) (string, error) {
-	if !checkEquality(args, []string{"./word_count", "file.txt"}) {
-		return "", errors.New(`Please provide two arguments: "./word_count file.txt"`)
-	}
-	return args[1], nil
-}
-
-func main() {
-	argsWithProg := os.Args
-	fileName, err := checkInput(argsWithProg...)
+	output, err := corpus.ParseFile(args[1])
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		output := corpus.ParseFile(fileName)
 		fmt.Println(output)
 	}
 }
