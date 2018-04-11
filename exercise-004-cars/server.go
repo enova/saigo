c860c3f5b3dd09f67d23d56cbb291050bd99041d
Joseph Klemen <jmklemen@gmail.com>
2018-04-11 11:39:08 -0500

    Additional separation of concerns.

--- exercise-004-cars/server.go
+++ exercise-004-cars/server.go
@@ -1,34 +1,22 @@
 package main
 
 import (
-  "html/template"
-  "net/http"
-  "runtime"
-  "path/filepath"
-  "fmt"
-)
-
-var (
-  PlayT *template.Template
-  JoinT *template.Template
-  root string
+	"fmt"
+	"net/http"
 )
 
 func setup() {
-  _, thisFile, _, _ := runtime.Caller(0)
-  root = filepath.Dir(thisFile)
-
-  JoinT = template.Must(template.ParseFiles(root + "/templates/join.html"))
-  PlayT = template.Must(template.ParseFiles(root + "/templates/play.html"))
+	SetupDir()
+	SetupTemplates()
+	SetupHandlers()
 }
 
 func main() {
-  setup()
-  SetupHandlers()
+	setup()
 
-  fs := http.FileServer(http.Dir(root + "/public/"))
-  http.Handle("/public/", http.StripPrefix("/public/", fs))
+	fs := http.FileServer(http.Dir(RootDir() + "/public/"))
+	http.Handle("/public/", http.StripPrefix("/public/", fs))
 
-  fmt.Println("Listening on :8086...")
-  http.ListenAndServe(":8086", nil)
+	fmt.Println("Listening on :8086...")
+	http.ListenAndServe(":8086", nil)
 }
--- exercise-004-cars/vehicles.go
+++ exercise-004-cars/vehicles.go
@@ -1,50 +1,56 @@
 package main
 
-var VehicleMap = make(map[string]*Vehicle, 0)
+var vmap = make(map[string]*Vehicle, 0)
 
+// Vehicle holds a vehicle name and count
 type Vehicle struct {
-  Name  string
-  Count int
+	Name  string
+	Count int
 }
 
+// Vehicles holds a slice of Vehicle structs
 type Vehicles struct {
-  List []*Vehicle
+	List []*Vehicle
 }
 
+// View holds a Vehicles and a Username for rendering
 type View struct {
-  Username string
-  Vehicles Vehicles
+	Username string
+	Vehicles Vehicles
 }
 
+// ViewVehicles generates a View for a given username
 func ViewVehicles(username string) View {
-  return View{
-    Username: username,
-    Vehicles: AsVehicles(),
-  }
+	return View{
+		Username: username,
+		Vehicles: AsVehicles(),
+	}
 }
 
+// AsVehicles returns the current map as Vehicles
 func AsVehicles() Vehicles {
-  vlist := make([]*Vehicle, len(VehicleMap))
-  i := 0
-  for _, vehicle := range VehicleMap {
-    if vehicle == nil {
-      continue
-    }
-    vlist[i] = vehicle
-    i++
-  }
-  return Vehicles{List: vlist}
+	vlist := make([]*Vehicle, len(vmap))
+	i := 0
+	for _, vehicle := range vmap {
+		if vehicle == nil {
+			continue
+		}
+		vlist[i] = vehicle
+		i++
+	}
+	return Vehicles{List: vlist}
 }
 
+// AddVehicle adds or updates the given model and speed in the map
 func AddVehicle(model string, speed string) {
-  name := model + ": " + speed
-  if v, e := VehicleMap[name]; !e {
-    veh := new(Vehicle)
-    veh.Name = name
-    veh.Count = 1
-    VehicleMap[name] = veh
-  } else {
-    v.Count++
-    VehicleMap[name] = v
-  }
+	name := model + ": " + speed
+	if v, e := vmap[name]; !e {
+		veh := new(Vehicle)
+		veh.Name = name
+		veh.Count = 1
+		vmap[name] = veh
+	} else {
+		v.Count++
+		vmap[name] = v
+	}
 }
