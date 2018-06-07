b84ef933ec689712f9fa532895f12f1e2ecaf327
Nicholas Sutanto <nsutanto@enova.com>
2018-06-07 11:54:26 -0500

    Implement Players

--- exercise-009-rock/src/rock/player.go
+++ exercise-009-rock/src/rock/player.go
@@ -4,17 +4,55 @@ import (
 	"math/rand"
 )
 
-// Player ...
-type Player struct {
+// RandoRex information
+type RandoRex struct {
+
 }
 
 // Type returns the type of the player
-func (p *Player) Type() string {
+func (p *RandoRex) Type() string {
 	return "RandoRex"
 }
 
 // Play returns a move
-func (p *Player) Play() int {
+func (p *RandoRex) Play() int {
 	choice := rand.Int() % 3
 	return choice
+}
+
+// PlayerB information
+type PlayerB struct {
+
+}
+
+// Type returns the type of the player
+func (p *PlayerB) Type() string {
+	return "PlayerB"
+}
+
+// Play returns a move
+func (p *PlayerB) Play() int {
+	choice := rand.Int() % 5
+	return choice
+}
+
+// PlayerC information
+type PlayerC struct {
+
+}
+
+// Type returns the type of the player
+func (p *PlayerC) Type() string {
+	return "PlayerC"
+}
+
+// Play returns a move
+func (p *PlayerC) Play() int {
+	choice := rand.Int() % 2
+	return choice
+}
+
+type Player interface {
+	Type() string
+	Play() int
 }
