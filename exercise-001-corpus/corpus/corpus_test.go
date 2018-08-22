bcde463ab86ff63ea7f99ca14bc0203d6c27aabd
Carlos Rocha <crocha@enova.com>
2018-08-22 11:08:01 -0500

    Add more tests

--- exercise-001-corpus/corpus/corpus_test.go
+++ exercise-001-corpus/corpus/corpus_test.go
@@ -12,6 +12,11 @@ func TestCorpus(t *testing.T) {
 	assert.Equal(t, result[0].Count, 2)
 }
 
+func TestEmptyText(t *testing.T) {
+	result := Analyze("")
+	assert.Equal(t, len(result), 0)
+}
+
 func TestKeepAppearanceOrder(t *testing.T) {
 	result := Analyze("stop words are great stop words are great")
 	assert.Equal(t, len(result), 4)
@@ -23,4 +28,15 @@ func TestKeepAppearanceOrder(t *testing.T) {
 	assert.Equal(t, result[2].Count, 2)
 	assert.Equal(t, result[3].Word, "great")
 	assert.Equal(t, result[3].Count, 2)
+}
+
+func TestCleansText(t *testing.T) {
+	result := Analyze("???????????!golang@#!@$!@#! words???+++ and more and more")
+	assert.Equal(t, len(result), 4)
+	assert.Equal(t, result[0].Word, "and")
+	assert.Equal(t, result[0].Count, 2)
+	assert.Equal(t, result[1].Word, "more")
+	assert.Equal(t, result[1].Count, 2)
+	assert.Equal(t, result[2].Word, "golang")
+	assert.Equal(t, result[2].Count, 1)
 }
