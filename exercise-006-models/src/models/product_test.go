114ba30a9c0c4c66ab586fb846c3be2f13d21a47
Eugene Magdel <emagdel@enova.com>
2018-09-19 11:33:40 -0500

    [exercise-006-models_v1] Implementing required functions and tests

--- exercise-006-models/src/models/product_test.go
+++ exercise-006-models/src/models/product_test.go
@@ -1,11 +1,25 @@
 package models
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-func TestProduct(t *testing.T) {
-	assert := assert.New(t)
-	assert.True(true)
+
+func init(){
+	Truncate()
+}
+
+func createValidProduct(productID int, name string) (*Product, error){
+	return NewProduct(GetConn(), productID, name)
+}
+
+func TestFindProduct(t *testing.T) {
+	defer Truncate()
+	productName := "test"
+	productNameSubstring := productName[0:2]
+	createValidProduct(1, productName)
+	product, _  := FindProduct(GetConn(), productNameSubstring)
+	assert.NotNil(t, product)
+	assert.Equal(t, product.Name, productName)
 }
