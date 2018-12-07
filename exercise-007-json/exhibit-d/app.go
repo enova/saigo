5973e921e5a9897f4336ed6ee3959182870aa85c
Kavya <knagabhushana@enova.com>
2018-12-07 13:32:35 -0600

    Json exercise saigo

--- exercise-007-json/exhibit-d/app.go
+++ exercise-007-json/exhibit-d/app.go
@@ -2,34 +2,41 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
 // Phone ...
 type Phone struct {
-	Name string `json:"name"`
+	Name 		string 	`json:"name"`
+	Age 		int 	`json:"age"`
+	ImageUrl 	string  `json:"imageUrl"`
+	ID 			string  `json:"id"`
+	Snippet		string  `json:"snippet"`
 }
 
 var allPhones []Phone
 
-func setup() {
-	data, err := ioutil.ReadFile("phones.json")
+func PanicOn(err error) {
 	if err != nil {
-		fmt.Println("Error reading phones.json")
-		os.Exit(1)
-	}
-
-	err = json.Unmarshal(data, &allPhones)
-	if err != nil {
-		fmt.Println("Error in unmarshalling phones")
+		panic(err)
 	}
 }
 
+func setup() {
+	data, err := ioutil.ReadFile("exhibit-d/phones.json")
+	PanicOn(err)
+
+	err = json.Unmarshal(data, &allPhones)
+	PanicOn(err)
+
+}
+
 func phones(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(allPhones)
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "    ")
+	err := enc.Encode(allPhones)
+	PanicOn(err)
 }
 
 func main() {
