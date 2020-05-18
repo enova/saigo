3a513cf3e24715193d2b5d3fdcb564c9aebe57c2
Vipul Bhat <vbhat@enova.com>
2020-05-17 19:43:28 -0500

    Add player types

--- exercise-009-rock/src/rock/player.go
+++ exercise-009-rock/src/rock/player.go
@@ -5,16 +5,63 @@ import (
 )
 
 // Player ...
-type Player struct {
+type Player interface {
+	Type() string
+	Play() int
+}
+
+type RandoRex struct {
 }
 
 // Type returns the type of the player
-func (p *Player) Type() string {
+func (r *RandoRex) Type() string {
 	return "RandoRex"
 }
 
 // Play returns a move
-func (p *Player) Play() int {
+func (r *RandoRex) Play() int {
 	choice := rand.Int() % 3
 	return choice
+}
+
+type Flipper struct {
+	move1 int
+	move2 int
+}
+
+func (f *Flipper) Type() string {
+	return "Flipper"
+}
+
+func (f *Flipper) Play() int {
+	moves := []int{f.move1, f.move2}
+	return moves[rand.Intn(2)]
+}
+
+type Obsessed struct {
+	move int
+}
+
+func (o *Obsessed) Type() string {
+	return "Obsessed"
+}
+
+func (o *Obsessed) Play() int {
+	return o.move
+}
+
+type Cyclone struct {
+	count int
+}
+
+func (c *Cyclone) Type() string {
+	return "RandoRex"
+}
+
+func (c *Cyclone) Play() int {
+	moves := []int{Rock, Paper, Scissors}
+	move := moves[c.count%3]
+	c.count++
+	return move
+
 }
